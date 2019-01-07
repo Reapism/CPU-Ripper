25cb97dca83ca3c9b92a11949775fdc80a239413
REAPIX\Reapism <reapsprgm@gmail.com>
2019-01-07 02:15:07 -0500

    creating directories

--- CPU-Ripper/window/MainWindow.xaml.cs
+++ CPU-Ripper/window/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CPU_Ripper {
+namespace CPU_Ripper.window {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
