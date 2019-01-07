9f25bc60a81c2fc655ab132a43a27357a1fb94a2
REAPIX\Reapism <reapsprgm@gmail.com>
2019-01-07 02:18:41 -0500

    +UnitTestRipper.cs - Performs the unit test using the Ripper and RipperSettings class and creates a score.

---
+++ CPU-Ripper/util/UnitTestRipper.cs
@@ -0,0 +1,22 @@
+﻿namespace CPU_Ripper.util {
+
+    /// <summary>
+    /// <para>The UnitTestRipper class.</para>
+    /// Performs a unit test on the Ripper class
+    /// and also functions for an interface for the
+    /// <see cref="Ripper"/> class.
+    /// <para>Author: Reapism / Anthony Jaghab (c), all rights reserved.</para>
+    /// </summary>
+
+    public class UnitTestRipper {
+
+        public UnitTestRipper() {
+
+        }
+
+        public bool RunTests() {
+
+        }
+
+    }
+}
