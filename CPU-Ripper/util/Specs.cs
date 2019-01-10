7b192679a80fa298fe6f40cb6a8fb720b2eee0c5
REAPIX\Reapism <reapsprgm@gmail.com>
2019-01-10 17:34:25 -0500

    Specs class, returns a string of system specs.

---
+++ CPU-Ripper/util/Specs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CPU_Ripper.util {
+    public class Specs {
+
+        public Specs() {
+
+        }
+
+        public override string ToString() =>
+          $"Machine Name: {Environment.MachineName}\n" +
+            $"User: {Environment.UserName}\n" +
+            $"Network: {Environment.UserDomainName}\n" +
+            $"System path: {Environment.SystemDirectory}\n" +
+            $"CLR version: {Environment.Version}\n" +
+            $"Ticks: {Environment.TickCount}\n" +
+            $"Platform: {Environment.OSVersion.Platform}\n" +
+            $"Service Pack: {Environment.OSVersion.ServicePack}\n" +
+            $"OS Version: {Environment.OSVersion.Version}\n" +
+            $"Threads: {Environment.ProcessorCount}\n";
+
+
+
+
+    }
+}
