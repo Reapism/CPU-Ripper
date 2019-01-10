22eb87b8aa5ac5d5dae2071b63318651b66148ca
REAPIX\Reapism <reapsprgm@gmail.com>
2019-01-10 17:27:15 -0500

    RipperThreadException class, represents a RipperthreadException

---
+++ CPU-Ripper/exception/RipperThreadException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CPU_Ripper.exception {
+
+    /// <summary>
+    /// The <see cref="RipperThreadException"/> class.
+    /// <para>Represents a <see cref="RipperThreadException"/> with a custom
+    /// messsage.</para>
+    /// </summary>
+
+    public class RipperThreadException : Exception {
+
+        private string exceptionMsg;
+
+        /// <summary>
+        /// Default parameterized constructor which holds 
+        /// a custom <seealso cref="RipperThreadException"/>
+        /// message.
+        /// </summary>
+        /// <param name="exceptionMsg">The custom 
+        /// <seealso cref="RipperThreadException"/> message.</param>
+
+        public RipperThreadException(string exceptionMsg) {
+            this.exceptionMsg = exceptionMsg;
+        }
+
+        /// <summary>
+        /// Returns a string describing this 
+        /// particular <see cref="RipperThreadException"/>.
+        /// </summary>
+        /// <returns>A string containing the <see cref="RipperThreadException"/></returns>
+
+        public override string ToString() => $"A RipperThreadException was generated. -> { this.exceptionMsg }.";
+
+
+    }
+}
