799fd75c7a7b582191e65f162a7f9e42a4653f40
REAPIX\Reapism <reapsprgm@gmail.com>
2019-01-12 03:19:11 -0500

    Added a Ripper and RipperSettings instance + void InitializeSettings() to initialize values from the application settings into the RipperSettings instance.

--- CPU-Ripper/window/MainWindow.xaml.cs
+++ CPU-Ripper/window/MainWindow.xaml.cs
@@ -1,25 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CPU_Ripper.util;
+using System;
+using System.Diagnostics;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Runtime.Serialization;
 
 namespace CPU_Ripper.window {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+    #region The mainwindow class.
+
     public partial class MainWindow : Window {
+
+        #region Instance members and properties
+
+        /// <summary>
+        /// A <see cref="RipperSettings"/> instance.
+        /// </summary>
+        private RipperSettings rs;
+
+        /// <summary>
+        /// A <see cref="Ripper"/> instance.
+        /// </summary>
+        private Ripper r;
+
+        #endregion
+
+        #region Constructors and methods.
+
+        /// <summary>
+        /// Default constructor for the main window.
+        /// </summary>
+
         public MainWindow() {
             InitializeComponent();
+
+            rs = new RipperSettings();
+            r = new Ripper(ref rs);
+      
+            InitializeSettings();
         }
+
+        /// <summary>
+        /// Initializes values in the current <see cref="RipperSettings"/>
+        /// instance to values stored in the settings. 
+        /// </summary>
+
+        private void InitializeSettings() {
+            this.rs.IterationsSuccessorship = Properties.Settings.Default.iter_successor;
+            this.rs.IterationsBoolean = Properties.Settings.Default.iter_bool;
+            this.rs.IterationsQueue = Properties.Settings.Default.iter_queue;
+            this.rs.IterationsLinkedList = Properties.Settings.Default.iter_linkedlist;
+            this.rs.IterationsTree = Properties.Settings.Default.iter_tree;
+
+            this.rs.AutoCheckForUpdates = Properties.Settings.Default.auto_updates;
+            this.rs.FluidLoading = Properties.Settings.Default.fluid_loading;
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e) {
+            r.SingleThread();
+            
+        }
+
+        private void BtnSpecs_Click(object sender, RoutedEventArgs e) {
+            MessageBox.Show(new Specs().ToString());
+        }
+
+        private void BtnCompare_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+
+
+
+        #endregion
     }
+
+    #endregion
 }
