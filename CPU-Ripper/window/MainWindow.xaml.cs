76dfa814c7eaae4ffcf7440eeb0d32f7b129925d
REAPIX\Reapism <reapsprgm@gmail.com>
2019-01-15 01:57:45 -0500

    Uses storyboards for btnStart

--- CPU-Ripper/window/MainWindow.xaml.cs
+++ CPU-Ripper/window/MainWindow.xaml.cs
@@ -4,14 +4,17 @@ using System.Diagnostics;
 using System.Threading;
 using System.Windows;
 using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
 
 namespace CPU_Ripper.window {
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    ///<para>Author: Reapism / Anthony Jaghab (c), all rights reserved.</para>
     /// </summary>
 
-    #region The mainwindow class.
+    #region The MainWindow class.
 
     public partial class MainWindow : Window {
 
@@ -60,9 +63,44 @@ namespace CPU_Ripper.window {
             this.rs.FluidLoading = Properties.Settings.Default.fluid_loading;
         }
 
+        
+
         private void BtnStart_Click(object sender, RoutedEventArgs e) {
-            r.SingleThread();
+            //// Create a timer and add its corresponding event
+            //System.Timers.Timer timer = new System.Timers.Timer();
+            //timer.Elapsed += TimerFade_Elapsed;
+
+            //timer.Interval = 5;
+
+            //// Want a new thread to run this task on so
+            //// the main thread doesn't wait.
+
+            //Task task = new Task(() => timer.Start());
+            //task.Start();          
+            //r.SingleThread();
+
+            //new RipperAnimation(ref this.btnStart).FadeOut(RipperAnimation.RipperTimer.FAST);
             
+            Storyboard sb = this.FindResource("FadeAnim") as Storyboard;
+            Storyboard.SetTarget(sb, this.btnStart);
+            sb.Begin();
+            
+        }
+
+        private void TimerFade_Elapsed(object sender, System.Timers.ElapsedEventArgs e) {
+            // Access UI thread to decrease Opacity on a button from a different thread.
+
+            Dispatcher.Invoke(() => {
+                if (btnStart.Opacity > 0.0) {
+                    btnStart.Opacity -= 0.01;
+                    // code here to force update the GUI.
+                } else {
+                    System.Timers.Timer t;
+                    t = (System.Timers.Timer)sender;
+                    t.Stop();
+                }
+            });          
+                
         }
 
         private void BtnSpecs_Click(object sender, RoutedEventArgs e) {
@@ -72,9 +110,6 @@ namespace CPU_Ripper.window {
         private void BtnCompare_Click(object sender, RoutedEventArgs e) {
 
         }
-
-
-
 
         #endregion
     }
